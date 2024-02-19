5d331925b474a66e265f9b174cba5481c20737db
PcMain <hital12.ci@gmail.com>
2024-02-19 13:14:49 -0300

    inicio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    REACT_APP_API_DOMAIN: "http://localhost:8000/api",
+    REACT_APP_IMG_DOMAIN: "http://localhost:8000/img",
+  },
+};
 
 export default nextConfig;
