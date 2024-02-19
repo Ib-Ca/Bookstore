d6c4da617ff49172caa3fb8324e2cbace9a427f6
PcMain <hital12.ci@gmail.com>
2024-02-19 17:41:20 -0300

    visual

---
+++ src/app/(loginyregis)/recover/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RecoverPassword = () => {
+  return (
+    <>
+      <h1>
+        RECOVER PASSWORD
+      </h1>
+    </>
+  );
+};
+
+export default RecoverPassword;
---
+++ src/components/navs/topNav.jsx
@@ -0,0 +1,40 @@
+'use client'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useRouter } from 'next/router';
+
+const TopNav=()=> {
+    const router=useRouter()
+    const handleRedirect=(route)=>()=>{
+        router.push(route)
+    }
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit" onClick={()=>handleRedirect("/login")}>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default TopNav
